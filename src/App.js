976b6bc63d2589cb40e4012807719423a43cf075
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2022-08-29 21:24:02 -0300

    interface OK

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import './styles/App.css';
 import './styles/PostForm.css';
 import './styles/Feed.css'
@@ -5,6 +7,7 @@ import './styles/Feed.css'
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
 import clockIcon from './images/clock.svg'
+import emptyFolderIcon from './images/empty-folder.svg'
 
 
 export default function App() {
@@ -15,14 +18,6 @@ export default function App() {
             userName: 'Samuel',
             publishedAt: new Date(),
         },
-
-        { //vai gerar um Id aleatório 
-            id: Math.random(),
-            content: 'Contéudo de post',
-            userName: 'Luana',
-            publishedAt: new Date(),
-        },
-
     ];
 
     return (
@@ -44,37 +39,48 @@ export default function App() {
             </form>
 
             <main>
-                <header>
-                    <h1>Seu Feed</h1>
-                    <h2>
-                        Acompanhe oque seus amigos estão pensado em tempo real
+                {posts.length === 0 ? <div className='feed-status'>
+                    <img src={emptyFolderIcon} alt='Empty Folder' />
+
+                    <h1>Não encontramos nada
+                    </h1>
+                    <h2>Parece que seus amigos não postaram nada. Comece a escrever uma nova história!
                     </h2>
-                </header>
-                <section className='feed'>
-                    {posts.map((posts) => (
-                        <article key={posts.id}>
-                            <p>
-                                {posts.content}
-                            </p>
+                </div> : null}
 
-                            <footer>
-                                <div className='user-details'>
-                                    <img src={userIcon} alt='User' />
-                                    <strong>{posts.userName}</strong>
-                                </div>
-                                <div className='time'>
-                                    <img src={clockIcon} alt='Clock' />
-                                    <span>
-                                        Publicado em {posts.publishedAt.toLocaleDateString('pt-br')}
-                                    </span>
-                                </div>
-                            </footer>
-                        </article>
-
-                    ))}
-
-                </section>
-            </main>
-        </div>
+                {posts.length > 0 && 
+                <>
+                    <header>
+                            <h1>Seu Feed</h1>
+                            <h2>
+                                Acompanhe oque seus amigos estão pensado em tempo real
+                            </h2>
+                     </header>
+                    
+                    <section className='feed'>
+                        {posts.map((posts) => (
+                            <article key={posts.id}>
+                                <p>
+                                    {posts.content}
+                                </p>
+                                <footer>
+                                    <div className='user-details'>
+                                        <img src={userIcon} alt='User' />
+                                        <strong>{posts.userName}</strong>
+                                    </div>
+                                    <div className='time'>
+                                        <img src={clockIcon} alt='Clock' />
+                                        <span>
+                                            Publicado em {posts.publishedAt.toLocaleDateString('pt-br')}
+                                        </span>
+                                    </div>
+                                </footer>
+                            </article>
+                        ))}
+                    </section>
+                </>
+                }
+            </main >
+        </div >
     );
 }
